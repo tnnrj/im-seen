8f83f1bf3754d81bc9b932fa0cd852313ea72e9a
Tanner Jorgensen <tannerj7332@gmail.com>
2021-02-28 19:09:54 -0700

    Added ui kit primevue

--- im-client/src/main.ts
+++ im-client/src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
-createApp(App).use(router).mount('#app')
+// primevue theming
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+// vue-router
+app.use(router);
+
+// primefaces
+app.use(PrimeVue);
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+app.component('InputText', InputText);
+
+app.mount('#app');
--- im-client/src/views/Home.vue
+++ im-client/src/views/Home.vue
@@ -2,6 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Button label="Show" icon="pi pi-external-link" @click="showDialog = true" />
+    <Dialog header="Header" v-model:visible="showDialog" :style="{width: '50vw'}">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <template #footer>
+            <Button label="No" icon="pi pi-times" @click="showDialog = false" class="p-button-text"/>
+            <Button label="Yes" icon="pi pi-check" @click="showDialog = false" autofocus />
+        </template>
+    </Dialog>
   </div>
 </template>
 
@@ -14,5 +25,7 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  showDialog = false;
+}
 </script>
