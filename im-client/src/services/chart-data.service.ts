81858a5b7373e056660c35914780d8ae59ca7cb1
Tanner Jorgensen <tannerj7332@gmail.com>
2021-04-12 18:49:07 -0600

    env fix

--- im-client/src/services/chart-data.service.ts
+++ im-client/src/services/chart-data.service.ts
@@ -1,4 +1,4 @@
 export function getChartData(queryId: string) {
-  // axios.get(process.env.API_URL + 'ChartData', queryId);
+  // axios.get(process.env.VUE_APP_API_URL + 'ChartData', queryId);
   return [queryId, queryId]
 }
