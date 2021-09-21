aebf8a47f442e696d5552c4110c4b014f72638cc
Tanner Jorgensen <tannerj7332@gmail.com>
2021-09-21 11:09:24 -0600

    Seeding change

--- im-server/IMWebAPI/Data/Observation_Seeding.cs
+++ im-server/IMWebAPI/Data/Observation_Seeding.cs
@@ -18,22 +18,22 @@ namespace IMWebAPI.Data
 
             var reports = new Observation[]
             {
-                new Observation {ObservationDate=new DateTime(1994,10,31,19,34,0),
+                new Observation {ObservationDate=new DateTime(2021,9,20,0,0,0),
                     UserName="Dumbledore",
                     StudentName=context.Students.Where(s => s.LastName == "Harry").Select(s => s.LastName).FirstOrDefault(),
                     Severity=4, Description="Put his own name into the Goblet of Fire even though he is underage. Recklessness is concerning."},
 
-                new Observation {ObservationDate=new DateTime(1991,10,31,18,12,0),
+                new Observation {ObservationDate=new DateTime(2021,9,19,0,0,0),
                     UserName="McGonagall",
                     StudentName=context.Students.Where(s => s.LastName == "Hermione").Select(s => s.LastName).FirstOrDefault(),
                     Severity=2, Description="I didn't see her at the Halloween Feast. But another student mentioned she was crying in the bathroom."},
 
-                new Observation {ObservationDate=new DateTime(1994,12,25,22,15,33),
+                new Observation {ObservationDate=new DateTime(2021,9,17,0,0,0),
                     UserName="Filch",
                     StudentName=context.Students.Where(s => s.LastName == "Hermione").Select(s => s.LastName).FirstOrDefault(),
                     Severity=1, Description="Towards the end of the night, she was crying on the staircase outside the ballroom. Possibly due to drama with a Weasley boy."},
 
-                new Observation {ObservationDate=new DateTime(1996,11,20,14,46,0),
+                new Observation {ObservationDate=new DateTime(2021,9,19,0,0,0),
                     UserName="Filch",
                     StudentName=context.Students.Where(s => s.LastName == "Draco").Select(s => s.LastName).FirstOrDefault(),
                     Severity=3, Description="He was sulking on the seventh floor left corridor, pacing back and forth and looking like he was up to no good. Then he disappeared."},
