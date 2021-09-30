f0873c7ed348be398b7221a4cd34ad96f8c1c411
Bao Chau Pham <baochaup@gmail.com>
2021-09-29 22:05:10 -0600

    Added more dummy data

---
+++ im-client/src/components/charts/BarChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :id="'chart-'+id" style="width:100%;height:100%;">
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+ name: "BarChart",
+  props: ['chartData', 'id'],
+  mounted() {
+    this.main();
+  },
+  methods: {
+    main() {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
