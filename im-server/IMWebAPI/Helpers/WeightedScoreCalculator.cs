cc7dc121d1257ffaa31ef7fda8c6cff159d62f28
Steven Carpadakis <Jcarpos4@gmail.com>
2021-11-02 18:51:09 -0600

    Started new class for weighted score calculation. (Not used yet)

---
+++ im-server/IMWebAPI/Helpers/WeightedScoreCalculator.cs
@@ -0,0 +1,34 @@
+﻿using IMWebAPI.Data;
+using IMWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMWebAPI.Helpers
+{
+    public class WeightedScoreCalculator
+    {
+        private readonly IM_API_Context _context;
+        private readonly double numDays;
+
+        public WeightedScoreCalculator(IM_API_Context context)
+        {
+            _context = context;
+            numDays = 7;
+        }
+
+        public double CalculateObservationFrequencyIncrease (Student student, double prevFrequency, out double newFrequency)
+        {
+            var observs = _context.Observations
+                .Where(o => o.StudentID == student.StudentID)
+                .Where(o => o.ObservationDate >= DateTime.Now.AddDays(-numDays)).ToList();
+
+            newFrequency = observs.Count / numDays;
+
+            return newFrequency / prevFrequency;
+        }
+    }
+}
