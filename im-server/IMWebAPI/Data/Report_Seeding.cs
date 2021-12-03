7e378fdbd0667af68edbc43ce102ab2a9f662845
Tanner Jorgensen <tannerj7332@gmail.com>
2021-12-02 21:32:27 -0700

    more small report fixes

--- im-server/IMWebAPI/Data/Report_Seeding.cs
+++ im-server/IMWebAPI/Data/Report_Seeding.cs
@@ -29,7 +29,7 @@ namespace IMWebAPI.Data
                 new Report
                 {
                     ReportName = "Observation Frequency by Student and Date",
-                    Query = "SELECT TRIM(CONCAT(StudentFirstName, ' ', StudentLastName)) as name, CAST(ObservationDate AS DATE) as date, COUNT(*) as value, Observations.StudentID as id FROM Observations {joinAndWhere} GROUP BY name, date, id ORDER BY ObservationDate",
+                    Query = "SELECT TRIM(CONCAT(StudentFirstName, ' ', StudentLastName)) as name, CAST(ObservationDate AS DATE) as date, COUNT(*) as value, Observations.StudentID as id FROM Observations {joinAndWhere} GROUP BY name, date, id ORDER BY date",
                     Axis1Name = "Date",
                     Axis2Name = "Frequency of Observations",
                     AvailableChartTypes = "Line"
@@ -37,14 +37,14 @@ namespace IMWebAPI.Data
                 new Report
                 {
                     ReportName = "Observation Severity by Student and Date",
-                    Query = "SELECT TRIM(CONCAT(StudentFirstName, ' ', StudentLastName)) as name, CAST(ObservationDate AS DATE) as date, SUM(Severity) as value, Observations.StudentID as id FROM Observations {joinAndWhere} GROUP BY name, date, id ORDER BY ObservationDate",
+                    Query = "SELECT TRIM(CONCAT(StudentFirstName, ' ', StudentLastName)) as name, CAST(ObservationDate AS DATE) as date, SUM(Severity) as value, Observations.StudentID as id FROM Observations {joinAndWhere} GROUP BY name, date, id ORDER BY date",
                     Axis1Name = "Date",
                     Axis2Name = "Severity of Observations",
                     AvailableChartTypes = "Line"
                 },
                 new Report {
                     ReportName = "Observations Grouped by Severity",
-                    Query = "SELECT Severity as name, COUNT(Severity) AS value FROM Observations {joinAndWhere} GROUP BY value",
+                    Query = "SELECT Severity as name, COUNT(Severity) AS value FROM Observations {joinAndWhere} GROUP BY name",
                     Axis1Name = "Severity",
                     Axis2Name = "Proportion",
                     AvailableChartTypes = "Bar,Pie,BubbleCloud"
